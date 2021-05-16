c941cc0dfdcc937440b7549056a6b4737689408b
aseelps <aseelpsathar@gmail.com>
2021-05-16 05:53:14 +0000

    juice: removed some stuffs
    Signed-off-by: aseelps <aseelpsathar@gmail.com>

--- dot_juice.mk
+++ dot_juice.mk
@@ -7,13 +7,10 @@
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
 # Inherit some common Dot stuff.
-WITH_GAPPS := true
 $(call inherit-product, vendor/dot/config/common.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_BLUR := true
-DOT_OFFICIAL := true
-
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := dot_juice
